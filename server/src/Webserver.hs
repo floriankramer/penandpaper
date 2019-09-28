e6be40fe20f7c1d641cf857fb2b95ecb475608e6
Florian Kramer <flo.kramer@arcor.de>
2019-09-28 18:55:51 +0200

    Added a key for the login and support for displaying images.

--- server/src/Webserver.hs
+++ server/src/Webserver.hs
@@ -11,6 +11,7 @@ import qualified Network.Wai as W (responseLBS
                                   , responseFile
                                   , Application
                                   , pathInfo
+                                  , queryString
                                   , Response)
 import qualified Network.Wai.Handler.Warp as W (run)
 import qualified Network.HTTP.Types as H (status200, status404)
@@ -19,21 +20,35 @@ import qualified Data.List as List
 import qualified Data.String as String 
 import qualified Data.Text as Text
 import qualified Data.ByteString.UTF8 as UTF8
+import  Data.ByteString (ByteString)
+import Debug.Trace (traceId, traceShow)
 
 start :: IO ()
 start = W.run 8080 handleRequest
+
+isKeyQuery :: (ByteString, Maybe ByteString) -> Bool
+isKeyQuery (a, b) =
+  if a == "key" then
+    case b of
+      Just v ->  v == "e7ngvq6JY1WR0gSsq-g6"
+      Nothing -> False
+  else False
+
 
 handleRequest :: W.Application
 handleRequest req f =
   let 
     pathlist = W.pathInfo req
     path = List.intercalate "/" $ map Text.unpack pathlist
+    qsa = filter isKeyQuery (W.queryString req)
+    hasKey = length qsa > 0
   in
-    if | path == "index.html" -> f $ fileToResponse path
+    if | path == "index.html" && hasKey -> f $ fileToResponse path
        | path == "main.js" -> f $ fileToResponse path
        | path == "main.css" -> f $ fileToResponse path
        | path == "util.js" -> f $ fileToResponse path
-       | path == "" -> f $ fileToResponse "index.html"
+       | path == "" && hasKey -> f $ fileToResponse "index.html"
+       | head pathlist == "images" && length pathlist == 2 -> f $ fileToResponse path
        | otherwise -> do
          putStrLn $ "404 for path: " ++ path
          f $ W.responseLBS H.status404 [(H.hContentType, "text/html")] "Page not found."
@@ -47,11 +62,21 @@ fileToResponse path = W.responseFile H.status200 [(H.hContentType, UTF8.fromStri
 pathToContentType :: String -> String
 pathToContentType path 
   | ending == ".js" = "applications/javascript"
+  | ending == ".css" = "applications/css"
+  | ending == ".png" = "image/png"
+  | ending == ".jpg" = "image/jpeg"
   | otherwise = "text/html"
   where ending = pathToEnding path
 
 pathFindLastDot :: String -> Maybe Int
-pathFindLastDot s = List.elemIndex '.' $ reverse s
+pathFindLastDot s = 
+  let
+    i = (List.elemIndex '.' $ reverse s)
+  in 
+    case i of 
+      Just j -> Just $ (length s) - j - 1
+      Nothing -> Nothing 
+
 
 pathToEnding :: String -> String
 pathToEnding path =
